f32a0a62adfd004bfb87d5671858cce186765de3
SoyMilk13 <119906029+SoyMilk13@users.noreply.github.com>
2023-04-13 16:43:23 -0400

    Updated changelog for version 1.8

--- scripts/info.js
+++ scripts/info.js
@@ -1,3 +1,4 @@
+new ChangelogSection('Version 1.8 Update:', 'Added the "Almanac"', null, null);
 new ChangelogSection('Version 1.7 Update:', 'Added announcements', 'General bug fixes and background optimizations.', null);
 new ChangelogSection('Version 1.6 Update:', 'Added animations when bombs are clicked.', null, null);
 new ChangelogSection('Version 1.5 Update:', 'Fixed bug where peppers acted as bombs.', 'Updated fruit scale-- They should now appear larger on-screen.', null);
@@ -6,7 +7,7 @@ new ChangelogSection('Version 1.3 Update:', 'Added "How to Play" section.', null
 new ChangelogSection('Version 1.2 Update:', 'Added "Changelog"', null, null);
 new ChangelogSection('Version 1.1 Update:', 'Added "Watermelons"', 'Fixed bug where fruits spawned off-screen.', null);
 
-new Announcement('Minor Update: Version 1.7.1', 'Fixed announcement scaling and solved bug where high scores did not save properly.');
+new Announcement('Version 1.8 Update: The Almanac is Here!', 'Added the almanac, a place where you can view information on each different fruit type.');
 
 new Version;
 
